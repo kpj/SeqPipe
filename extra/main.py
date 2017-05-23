804ca7dd044b3ea277669ba19f7a9bc5d256b857
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-05-24 01:18:16 +0200

    Add parameter to plot absolute mapping counts

--- extra/main.py
+++ extra/main.py
@@ -14,13 +14,14 @@ def cli() -> None:
     pass
 
 @cli.command()
+@click.option('--absolute/--no-absolute', default=False)
 @click.argument('files', nargs=-1, type=click.Path(exists=True))
-def read_distribution(files: Sized) -> None:
+def read_distribution(absolute: bool, files: Sized) -> None:
     if len(files) == 0:
         print('No files provided')
         return
 
-    plot_distribution(files)
+    plot_distribution(files, absolute)
 
 
 if __name__ == '__main__':
--- extra/mapping_overview.py
+++ extra/mapping_overview.py
@@ -30,24 +30,25 @@ def compute_relative_counts(
     row['rel_count'] = row['mapped_count'] / total_align_num[row['read_name']]
     return row
 
-def read_distribution(df: pd.DataFrame) -> None:
+def read_distribution(df: pd.DataFrame, absolute: bool) -> None:
     """ Plot read fractions mapped to references
     """
     total_align_num = df.groupby('read_name').sum()['mapped_count'].to_dict()
     df = df.apply(compute_relative_counts, axis=1, args=(total_align_num,))
     df.to_csv('images/read_distribution.csv')
 
-    sub = df.pivot('read_name', 'reference')['rel_count']
+    count_ref = 'mapped_count' if absolute else 'rel_count'
+    sub = df.pivot('read_name', 'reference')[count_ref]
     sub.plot(kind='bar', stacked=True)
 
     plt.tight_layout()
     plt.savefig('images/read_distribution.pdf')
 
-def main(files: Sized) -> None:
+def main(files: Sized, absolute: bool = False) -> None:
     ensure_sanity()
     df = compute_statistics(files)
 
-    read_distribution(df)
+    read_distribution(df, absolute)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
