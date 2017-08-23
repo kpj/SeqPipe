df30cf4c201d0596567cbfc63a72d706ba3f56b8
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-08-23 12:01:52 +0200

    Bump version (0.0.6 -> 0.0.7)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='seqpipe',
-    version='0.0.6',
+    version='0.0.7',
 
     description='Sequencing pipeline',
     long_description_markdown_filename='README.md',
